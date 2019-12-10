7ed4662c87205829c835cf5f3b0395300d39488e
Chung <Chung@192.168.0.3>
2019-12-10 23:02:14 +0900

    Staring Rhombus

--- StaringPjt/src/StaringPT/Staring.java
+++ StaringPjt/src/StaringPT/Staring.java
@@ -3,26 +3,29 @@ package StaringPT;
 public class Staring {
 	/*
 	 * result : 
+	 *                  *
+	 *                * * *
+	 *              * * * * *
 	 *            * * * * * * *  
 	 *              * * * * *
 	 *                * * *
 	 *                  * 
 	 */
 	public static void main(String[] args) {
-//		for (int i = 0; i < 4; i++) {
-//			for (int j = 3; j > i; j--) {
-//				System.out.print("  ");
-//			}
-//			for (int j = 0; j <= 2*i; j++) {
-//				System.out.print("* ");
-//			}
-//			System.out.println();
-//		}
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < i; j++) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 3; j > i; j--) {
 				System.out.print("  ");
 			}
-			for (int j = 6; j >= 2*i; j--) {
+			for (int j = 0; j <= 2*i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("  ");
+			}
+			for (int j = 4; j >= 2*i; j--) {
 				System.out.print("* ");
 			}
 			System.out.println();
