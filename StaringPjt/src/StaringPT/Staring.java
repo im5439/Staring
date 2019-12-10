23d708aed80e061037fbccf43f93d9d0d05a9df6
Chung <Chung@192.168.0.3>
2019-12-10 22:54:30 +0900

    Staring Equilateral Triangle

--- StaringPjt/src/StaringPT/Staring.java
+++ StaringPjt/src/StaringPT/Staring.java
@@ -4,17 +4,16 @@ public class Staring {
 	/*
 	 * result : 
 	 *                  *  
-	 *                * *
-	 *              * * *
-	 *            * * * *
-	 *          * * * * *
+	 *                * * *
+	 *              * * * * *
+	 *            * * * * * * *
 	 */
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-			for (int j = 4; j > i; j--) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 3; j > i; j--) {
 				System.out.print("  ");
 			}
-			for (int j = 0; j <= i; j++) {
+			for (int j = 0; j <= 2*i; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
