30fb8f1f5f93a982c4b80d8b8a24a74913f0993f
Chung <Chung@192.168.0.3>
2019-12-10 22:30:45 +0900

    Staring Right Triangle

--- StaringPjt/src/StaringPT/Staring.java
+++ StaringPjt/src/StaringPT/Staring.java
@@ -3,15 +3,15 @@ package StaringPT;
 public class Staring {
 	/*
 	 * result : 
-	 *          * * * * * 
-	 *          * * * * * 
-	 *          * * * * *
-	 *          * * * * * 
+	 *          *  
+	 *          * *  
+	 *          * * * 
+	 *          * * * * 
 	 *          * * * * *
 	 */
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
